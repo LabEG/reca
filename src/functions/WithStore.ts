0dce7d0a05c08a995a830eae136b1d72ee66cdb9
Evgeny Labutin <labeg@mail.ru>
2022-04-16 00:04:51 +0300

    move IWithStore to separated file

--- src/functions/WithStore.ts
+++ src/functions/WithStore.ts
@@ -1,35 +1,10 @@
+import { IWithStore } from "./../interfaces/IWithStore";
 import {useStore} from "../hooks/UseStore";
 import { IDiClassCostructor } from "../interfaces/IClassCostructor";
 import {Store} from "../stores/Store";
 
 interface IStoreConstructor<P extends Record<string, unknown>> {
     new (...params: IDiClassCostructor[]): Store<P>;
-}
-
-
-interface IWithStore<P extends Record<string, unknown> = {}> {
-    // 0 stores
-    (
-        view: (props?: P) => JSX.Element | null
-    ): (props?: P) => JSX.Element | null;
-
-    // 1 store
-    <
-        T1 extends Store<P>
-    >(
-        store1: new (...params: IDiClassCostructor[]) => T1,
-        view: (store1: T1, props?: P) => JSX.Element | null
-    ): (props?: P) => JSX.Element | null;
-
-    // 2 store
-    <
-        T1 extends Store<P>,
-        T2 extends Store<P>
-    >(
-        store1: new (...params: IDiClassCostructor[]) => T1,
-        store2: new (...params: IDiClassCostructor[]) => T2,
-        view: (store1: T1, store2: T2, props?: P) => JSX.Element | null
-    ): (props?: P) => JSX.Element | null;
 }
 
 export const withStore: IWithStore = <P extends Record<string, unknown>>(
