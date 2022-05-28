9c5ea11de04a8bbb0ab557547edab69a99384b87
Evgeny Labutin <labeg@mail.ru>
2022-05-28 23:30:57 +0300

    fix: module import

--- src/stores/Store.ts
+++ src/stores/Store.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-import {config} from "../config";
+import {config} from "../config.js";
 
 export class Store<T extends object = object> {
 
