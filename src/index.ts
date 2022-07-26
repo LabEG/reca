78330ccb62451c199bc0688f312990f02a35592d
Evgeny Labutin <labeg@mail.ru>
2022-07-26 15:05:51 +0300

    fix: exports decorators for module type

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ export * from "./hooks/UseStore.js";
 export * from "./stores/Store.js";
 export * from "./stores/AutoStore.js";
 
-export * from "./decorators/NotAuto";
-export * from "./decorators/NotRedraw";
+export * from "./decorators/NotAuto.js";
+export * from "./decorators/NotRedraw.js";
 
 export {reflection} from "first-di";
 export * from "./config.js";
