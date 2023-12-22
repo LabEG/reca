33fdf7e4927c87144c1e4c7e70deef6b94a686ce
Evgeny Labutin <labeg@mail.ru>
2023-12-22 22:34:13 +0300

    refactor: prepare reca-docs for publishing on github pages

--- packages/reca-docs/app/page.tsx
+++ packages/reca-docs/app/page.tsx
@@ -1,95 +1,134 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
-export default function Home() {
-  return (
+// eslint-disable-next-line max-lines-per-function
+const Home = (): JSX.Element => (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <div className={styles.description}>
+            <p>
+                Get started by editing
+                {" "}
+
+                <code className={styles.code}>
+                    app/page.tsx
+                </code>
+            </p>
+
+            <div>
+                <a
+                    href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                >
+                    By
+                    {" "}
+
+                    <Image
+                        alt="Vercel Logo"
+                        className={styles.vercelLogo}
+                        height={24}
+                        priority
+                        src="/vercel.svg"
+                        width={100}
+                    />
+                </a>
+            </div>
         </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className={styles.center}>
+            <Image
+                alt="Next.js Logo"
+                className={styles.logo}
+                height={37}
+                priority
+                src="/next.svg"
+                width={180}
+            />
+        </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        <div className={styles.grid}>
+            <a
+                className={styles.card}
+                href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                rel="noopener noreferrer"
+                target="_blank"
+            >
+                <h2>
+                    Docs
+                    {" "}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+                    <span>
+                        -&gt;
+                    </span>
+                </h2>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+                <p>
+                    Find in-depth information about Next.js features and API.
+                </p>
+            </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+            <a
+                className={styles.card}
+                href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                rel="noopener noreferrer"
+                target="_blank"
+            >
+                <h2>
+                    Learn
+                    {" "}
+
+                    <span>
+                        -&gt;
+                    </span>
+                </h2>
+
+                <p>
+                    Learn about Next.js in an interactive course with&nbsp;quizzes!
+                </p>
+            </a>
+
+            <a
+                className={styles.card}
+                // eslint-disable-next-line max-len, @stylistic/max-len
+                href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                rel="noopener noreferrer"
+                target="_blank"
+            >
+                <h2>
+                    Templates
+                    {" "}
+
+                    <span>
+                        -&gt;
+                    </span>
+                </h2>
+
+                <p>
+                    Explore starter templates for Next.js.
+                </p>
+            </a>
+
+            <a
+                className={styles.card}
+                href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                rel="noopener noreferrer"
+                target="_blank"
+            >
+                <h2>
+                    Deploy
+                    {" "}
+
+                    <span>
+                        -&gt;
+                    </span>
+                </h2>
+
+                <p>
+                    Instantly deploy your Next.js site to a shareable URL with Vercel.
+                </p>
+            </a>
+        </div>
     </main>
-  )
-}
+);
+
+export default Home;
