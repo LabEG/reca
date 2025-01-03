3d175bfb34cdc92ec6a0c818facbe341f38177da
Eugene Labutin <labeg@mail.ru>
2025-01-04 02:05:28 +0300

    chore: minor fixes

--- packages/reca-docs/app/layout.tsx
+++ packages/reca-docs/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import "./globals.css";
+import {JSX} from "react";
 
 // eslint-disable-next-line new-cap
 const inter = Inter({subsets: ["latin"]});
